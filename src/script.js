b3eefb3bb8eca42bedfe91c6f24c7453a5801859
Akandra-Pawitra <akandrapawitra@gmail.com>
2023-01-07 08:45:38 +0700

    Add Ship factory

--- src/script.js
+++ src/script.js
@@ -1,3 +1,19 @@
-export const script = () => {
-  return 'test'
+const Ship = (length) => {
+  if (length < 2 || length > 5) {
+    return 'Invalid length'
+  } else {
+    const hits = 0
+    const sunk = 0
+    const hit = function () {
+      this.hits++
+    }
+    const isSunk = function () {
+      if (this.hits === this.length) this.sunk++
+      return Boolean(this.sunk)
+    }
+    return { length, hits, sunk, hit, isSunk }
+  }
 }
+
+const Script = { Ship }
+export default Script
--- src/script.test.js
+++ src/script.test.js
@@ -1,6 +1,24 @@
-// run <npx jest --watch> for test in each save
-import { script } from './script'
+import Script from './script'
 
-it('test', () => {
-  expect(script()).toBe('test')
+describe('Ship factory function', () => {
+  const ship = Script.Ship(2)
+  ship.hit()
+  const prevhit = ship.hits
+  ship.hit()
+
+  test('Must return an object', () => {
+    expect(typeof (ship)).toBe('object')
+  })
+
+  test('Length must be between 2 and 5', () => {
+    expect(Script.Ship(1) && Script.Ship(6)).toBe('Invalid length')
+  })
+
+  test('Total hits increase after being hit', () => {
+    expect(ship.hits).not.toEqual(prevhit)
+  })
+
+  test('Sunk if hits equal ship length', () => {
+    expect(ship.isSunk()).toBeTruthy()
+  })
 })
